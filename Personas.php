c9899566e7e2dd630c21bc7d30ca5cd17ae67389
lorena <lore.marmori.23@gmail.com>
2017-08-31 17:06:09 -0300

    nuevos cambios hechos

--- Personas.php
+++ Personas.php
@@ -5,7 +5,18 @@
 		public function hablar(){	
 	      echo "soy  " .$this->nombre. "  y estoy hablando";
 		}
+		public function acariciar($perrito){
+		   $perrito->moverCola();
+		}
 	}
+	
+	class perro{
+	    public $nombre;
+		public function moverCola(){
+			echo "muevo la Cola";		
+		}
+	}
+	
 	
 	$p = new Persona();
 	$p->nombre = "micaela";
@@ -17,10 +28,20 @@
 	echo "<br>";
 	echo "hola ".$p->nombre."<br>";
 	echo "<br>";
+	echo "<br>";
 	echo "hola ".$p1->nombre."<br>";
 	
 	$p->hablar();
 	echo "<br>";
 	$p1->hablar();
+	
+	$perro = new perro();
+	echo "<br>";
+	echo "<br>";
+	$perro-> moverCola();
+	echo "<br>";
+	echo "<br>";
+	$p->acariciar($perro);
+	
 	
 ?>
